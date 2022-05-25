7deab205b8d57d4f729c581c142ae565b11d8df5
Djibril <Gassama@MacBook-Pro-de-djibril.local>
2022-05-25 14:52:34 +0200

    auth features done ✅

--- app/app.py
+++ app/app.py
@@ -1,10 +1,28 @@
-import flask
-from flask import Flask
+from functools import wraps
+
+from flask import Flask, request, jsonify
+import jwt
 
 app = Flask(__name__)
-headers = flask.request.headers
-bearer = headers.get('Authorization')  # Bearer YourTokenHere
-token = bearer.split()[1]  # YourTokenHere
+
+app.config['SECRET_KEY'] = 'f38675ce86190d1196a15861c4d85e2f'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+        if token != app.config['SECRET_KEY']:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(*args, **kwargs)
+
+    return decorator
 
 
 @app.route("/")
@@ -18,6 +36,7 @@ def health():
 
 
 @app.route("/e", methods=["GET", "POST"])
+@token_required
 def hello_esgi():
     return "<p>Hello, ESGI !</p>"
 
